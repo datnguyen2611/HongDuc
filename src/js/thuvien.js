2eb36a1817c956695a72138740e35b2eaf32f7aa
Coconut.boi <mioriaty@gmail.com>
2019-07-09 17:14:51 +0700

    chinh fotoer

--- src/js/thuvien.js
+++ src/js/thuvien.js
@@ -1,25 +1,24 @@
 $(function () {
-    var addStickySidebar = function() {
-        var menuSidebar = $('#sidebar-wrap');
-        if(menuSidebar){
-            menuSidebar.offset().top;
-            if ($(window).width() > 991) {
-                $(window).scroll(function () {
-                    if ($(window).scrollTop() >= menuSidebar)  {
-                        $("#sidebar-wrap").addClass('sticky-sidebar');
-                    } else {
-                        $("#sidebar-wrap").removeClass('sticky-sidebar');
-                    }
-                    
-                });
-            } else {
-                return false;
-            }
+    var addStickySidebar = function () {
+        var menuSidebar = $('#sidebar-wrap').offset().top;
+        if ($(window).width() > 991) {
+            $(window).scroll(function () {
+                if ($(window).scrollTop() >= menuSidebar) {
+                    $("#sidebar-wrap").addClass('sticky-sidebar');
+                } else {
+                    $("#sidebar-wrap").removeClass('sticky-sidebar');
+                }
+
+            })
+        } else {
+            return false;
         }
+
+
     };
     addStickySidebar();
-    $(window).resize(function() {
+    $(window).resize(function () {
         addStickySidebar();
     });
-    
+
 });
